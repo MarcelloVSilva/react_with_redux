631c7328ea96f743d9821357d8e77651534682f0
MarcelloVSilva <marcellovcs@gmail.com>
2018-10-10 15:53:22 -0300

    arrow functions

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,88 +1,20 @@
 "use strict";
 
-console.log('teste');
-
-// JSX - JavaScript XML
-
-var template = React.createElement(
-    "div",
-    null,
-    React.createElement(
-        "h1",
-        null,
-        "Marcello"
-    ),
-    React.createElement(
-        "p",
-        null,
-        "This is some info here"
-    ),
-    React.createElement(
-        "ol",
-        null,
-        React.createElement(
-            "li",
-            null,
-            " Item one "
-        ),
-        React.createElement(
-            "li",
-            null,
-            " Item two "
-        )
-    )
-);
-
-var user = {
-    name: "Marcello",
-    age: 23,
-    location: "Goiânia - Goiás - Brazil"
-};
-
-function getLocation(location) {
-    if (location) return React.createElement(
-        "p",
-        null,
-        "My location is ",
-        location
-    );
+function square(x) {
+    return x * x;
 }
 
-var templateTwo = React.createElement(
-    "div",
-    null,
-    React.createElement(
-        "h1",
-        null,
-        "There is the templateTwo"
-    ),
-    React.createElement(
-        "h1",
-        null,
-        user.name ? user.name : 'Ninguém'
-    ),
-    user.age && user.age >= 23 && React.createElement(
-        "p",
-        null,
-        "My age is ",
-        user.age
-    ),
-    getLocation(user.location),
-    React.createElement(
-        "ol",
-        null,
-        React.createElement(
-            "li",
-            null,
-            " Item one "
-        ),
-        React.createElement(
-            "li",
-            null,
-            " Item two "
-        )
-    )
-);
-var appRoot = document.getElementById("app");
+console.log(square(3));
 
-ReactDOM.render(templateTwo, appRoot);
+var squareArrow = function squareArrow(x) {
+    return x * x;
+};
+
+console.log(squareArrow(9));
+
+var getFirstName = function getFirstName(_fullName) {
+    return _fullName.split(' ')[0];
+};
+
+var fullName = "Marcello Victor";
+console.log(getFirstName(fullName));
