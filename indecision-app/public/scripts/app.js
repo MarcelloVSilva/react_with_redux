144300a6e4ca6d06015b05b248797c03310b021d
MarcelloVSilva <marcellovcs@gmail.com>
2018-10-10 15:14:05 -0300

    dynamic vars ins basic jsx

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -5,10 +5,80 @@ console.log('teste');
 // JSX - JavaScript XML
 
 var template = React.createElement(
-  "h1",
-  null,
-  "This is jsx Marcello"
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "Marcello"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "This is some info here"
+    ),
+    React.createElement(
+        "ol",
+        null,
+        React.createElement(
+            "li",
+            null,
+            " Item one "
+        ),
+        React.createElement(
+            "li",
+            null,
+            " Item two "
+        )
+    )
+);
+
+var user = {
+    name: "Marcello",
+    age: 23,
+    location: "Brazil"
+};
+
+var templateTwo = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "There is the templateTwo"
+    ),
+    React.createElement(
+        "h1",
+        null,
+        user.name
+    ),
+    React.createElement(
+        "p",
+        null,
+        "My age is ",
+        user.age
+    ),
+    React.createElement(
+        "p",
+        null,
+        "My location is ",
+        user.location
+    ),
+    React.createElement(
+        "ol",
+        null,
+        React.createElement(
+            "li",
+            null,
+            " Item one "
+        ),
+        React.createElement(
+            "li",
+            null,
+            " Item two "
+        )
+    )
 );
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -2,7 +2,36 @@ console.log('teste')
 
 // JSX - JavaScript XML
 
-var template = <h1>This is jsx Marcello</h1>;
+var template = (
+    <div>
+        <h1>Marcello</h1>
+        <p>This is some info here</p>
+        <ol>
+            <li> Item one </li>
+            <li> Item two </li>
+        </ol>
+    </div>
+);
+
+var user = {
+    name: "Marcello",
+    age: 23,
+    location: "Brazil"
+}
+
+var templateTwo = (
+    <div>
+
+        <h1>There is the templateTwo</h1>
+        <h1>{user.name}</h1>
+        <p>My age is {user.age}</p>
+        <p>My location is {user.location}</p>
+        <ol>
+            <li> Item one </li>
+            <li> Item two </li>
+        </ol>
+    </div>
+);
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot)
+ReactDOM.render(templateTwo, appRoot)
